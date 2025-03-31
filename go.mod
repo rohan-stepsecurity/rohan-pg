0fc9430382e7a25b8159f5b4d90e5a88bf43ff7f
Rohan Prabhu <rohan@stepsecurity.io>
2025-03-31 12:11:56 +0530

    add ffiles

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rohan-stepsecurity/rohan-pg
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, GoReleaser!")
+	fmt.Println("Hello, GoReleaser! With no source code")
+}
